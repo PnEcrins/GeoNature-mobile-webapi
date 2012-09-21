2ed3746f8cbf353fc84ef98580189383eb7adb4b
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2012-09-21 15:48:12 +0200

    WGSI config

---
+++ faune/README.rst
@@ -0,0 +1,121 @@
+*Faune Webapi*
+
+=====
+SETUP
+=====
+
+Requirements
+------------
+
+* Ubuntu Server 12.04 Precise Pangolin (http://releases.ubuntu.com/12.04/)
+
+
+Installation
+------------
+
+Once the OS is installed (basic installation, with OpenSSH server), copy and extract the source archive.
+
+cd /path_to_webapi/faune/
+export FAUNE_PROJECT=`pwd`
+
+Set the Virtual env
+-------------------
+
+cd $FAUNE_PROJECT
+virtualenv --no-site-packages .
+
+Activate virtualenv
+-------------------
+cd $FAUNE_PROJECT
+source $FAUNE_PROJECT/bin/activate
+
+Install django
+--------------
+cd $FAUNE_PROJECT
+# download Django : https://www.djangoproject.com/download/1.4.1/tarball/
+tar xzvf Django-1.4.1.tar.gz
+rm Django-1.4.1.tar.gz
+cd Django-1.4.1
+sudo python setup.py install
+easy_install psycopg2
+
+Install wepapi
+--------------
+
+Copy source code to $FAUNE_PROJECT/faune/
+
+
+Configure apache vhost
+----------------------
+
+sudo apt-get install libapache2-mod-wsgi
+
+Create a new file :
+
+vi /etc/apache2/sites-available/faune
+
+containing :
+
+    WSGIScriptAlias /faune /path_to_webapi/faune/faune/wsgi.py
+    WSGIPythonPath /path_to_webapi/faune/faune/lib/python2.7/site-packages
+    <Directory /path_to_webapi/faune/>
+        <Files wsgi.py>
+            Order deny,allow
+            Allow from all
+        </Files>
+    </Directory>
+
+Replace /path_to_webapi/faune/ by the correct path.
+
+Then, create a link :
+
+    sudo ln -s /etc/apache2/sites-available/faune /etc/apache2/sites-enabled/faune
+
+And restart apache :
+
+    sudo apache2ctl restart
+
+
+Webapi is now available on http://localhost/faune/.
+
+URLs:
+-----
+
+Export data:
+http://localhost/faune/export/family
+http://localhost/faune/export/taxon
+http://localhost/faune/export/taxon_unity
+http://localhost/faune/export/criterion
+http://localhost/faune/export/unity
+http://localhost/faune/export/user
+
+POST parameter: 
+    token
+
+Import data:
+http://localhost/faune/import_data
+
+POST parameter: 
+    token
+    data (json data for importing)
+
+
+
+=======
+AUTHORS
+=======
+
+    * Sylvain Beorchia
+    * Mathieu Leplatre
+
+|makinacom|_
+
+.. |makinacom| image:: http://depot.makina-corpus.org/public/logo.gif
+.. _makinacom:  http://www.makina-corpus.com
+
+
+=======
+LICENSE
+=======
+
+    * (c) Makina Corpus
--- faune/faune/views.py
+++ faune/faune/views.py
@@ -5,6 +5,7 @@ from django.template import RequestContext, loader
 from django.utils import simplejson
 from django.utils.datastructures import SortedDict
 from django.conf import settings
+from django.utils.translation import ugettext as _
 
 from faune.utils import sync_db, query_db, commit_transaction
 
@@ -21,11 +22,20 @@ def import_data(request):
     response_content = []
         
     params = request.POST
-    data = params['data']
+    if params:
+        data = params['data']
+    else:
+        response_content.append({
+            'status' : _("No POST param given")
+        })
+        response = HttpResponse()
+        simplejson.dump(response_content, response,
+                    ensure_ascii=False, separators=(',', ':'))                    
+        return response
     
     if not check_token(request):
         response_content.append({
-            'status' : "You're not allowed to use this webservice"
+            'status' : _("You're not allowed to use this webservice")
         })
         response = HttpResponse()
         simplejson.dump(response_content, response,
@@ -44,16 +54,20 @@ def import_data(request):
         new_feature[json_to_db.get('dateobs')] = json_data['dateobs']
         new_feature[json_to_db.get('initial_input')] = json_data['initial_input']
         new_feature['supprime'] = 'False'
-        new_feature['id_protocole']= 140 # pour contact faune TODO: recuperer ces ID en dynamique, 142 pour mortalite
         new_feature['id_organisme']= 2  # (parc national des ecrins = fk de utilisateurs.bib_organismes)
-        new_feature['id_lot'] = 4 # 4 pour contact faune ; 15 pour mortalite
+        if json_data['input_type'] == 'fauna':
+            new_feature['id_protocole']= 140 # faune TODO: recuperer ces ID en dynamique, 142 pour mortalite
+            new_feature['id_lot'] = 4 # faune
+        else:
+            new_feature['id_protocole']= 142 # mortality
+            new_feature['id_lot'] = 15 # mortality
 
         # we need to transform into 2154
         new_feature[json_to_db.get('geometry')] = "transform(ST_GeomFromText('POINT(%s %s)', 4326),2154)" % (json_data['geolocation']['longitude'], json_data['geolocation']['latitude'])
         new_feature[json_to_db.get('accuracy')] = json_data['geolocation']['accuracy']
         new_feature[json_to_db.get('altitude')] = json_data['geolocation']['altitude']
         objects.append(new_feature)
-        cursor = sync_db(objects, False)
+        cursor = sync_db(objects)
         #id_fiche = cursor.fetchone()[0]
 
         # Insert into TABLE_STATEMENT
@@ -66,17 +80,27 @@ def import_data(request):
         new_feature[settings.FAUNE_TABLE_INFOS.get(settings.TABLE_SHEET).get('id_col')] = json_data['id']
         new_feature[json_to_db.get('id')] = json_data['taxon']['id']
         new_feature[json_to_db.get('name_entered')] = json_data['taxon']['name_entered']
-        new_feature[json_to_db.get('adult_male')] = json_data['taxon']['counting']['adult_male']
-        new_feature[json_to_db.get('adult_female')] = json_data['taxon']['counting']['adult_female']
-        new_feature[json_to_db.get('adult')] = json_data['taxon']['counting']['adult']
-        new_feature[json_to_db.get('not_adult')] = json_data['taxon']['counting']['not_adult']
-        new_feature[json_to_db.get('young')] = json_data['taxon']['counting']['young']
-        new_feature[json_to_db.get('yearling')] = json_data['taxon']['counting']['yearling']
-        new_feature[json_to_db.get('sex_age_unspecified')] = json_data['taxon']['counting']['sex_age_unspecified']
-        new_feature[json_to_db.get('criterion')] = json_data['taxon']['observation']['criterion']
-
+        if json_data['input_type'] == 'fauna':
+            new_feature[json_to_db.get('adult_male')] = json_data['taxon']['counting']['adult_male']
+            new_feature[json_to_db.get('adult_female')] = json_data['taxon']['counting']['adult_female']
+            new_feature[json_to_db.get('adult')] = json_data['taxon']['counting']['adult']
+            new_feature[json_to_db.get('not_adult')] = json_data['taxon']['counting']['not_adult']
+            new_feature[json_to_db.get('young')] = json_data['taxon']['counting']['young']
+            new_feature[json_to_db.get('yearling')] = json_data['taxon']['counting']['yearling']
+            new_feature[json_to_db.get('sex_age_unspecified')] = json_data['taxon']['counting']['sex_age_unspecified']
+            new_feature[json_to_db.get('criterion')] = json_data['taxon']['observation']['criterion']
+        else :
+            new_feature[json_to_db.get('adult_male')] = json_data['taxon']['mortality']['adult_male']
+            new_feature[json_to_db.get('adult_female')] = json_data['taxon']['mortality']['adult_female']
+            new_feature[json_to_db.get('adult')] = json_data['taxon']['mortality']['adult']
+            new_feature[json_to_db.get('not_adult')] = json_data['taxon']['mortality']['not_adult']
+            new_feature[json_to_db.get('young')] = json_data['taxon']['mortality']['young']
+            new_feature[json_to_db.get('yearling')] = json_data['taxon']['mortality']['yearling']
+            new_feature[json_to_db.get('sex_age_unspecified')] = json_data['taxon']['mortality']['sex_age_unspecified']
+            new_feature[json_to_db.get('sample')] = json_data['taxon']['mortality']['sample']
+        
         objects.append(new_feature)
-        cursor = sync_db(objects, False)
+        cursor = sync_db(objects)
         
         # Insert into TABLE_SHEET_ROLE
         objects = []
@@ -87,14 +111,14 @@ def import_data(request):
         new_feature['id_cf'] = json_data['id']
         new_feature['id_role'] = json_data['observer_id']
         objects.append(new_feature)
-        sync_db(objects, False)
+        sync_db(objects)
 
         # Commit transaction
         commit_transaction();
 
         response_content.append({
             #'status' : "Import done - statement id = %s, sheet id = %s" % (id_releve, id_fiche)
-            'status' : "Import done - statement id = %s, sheet id = %s" % (json_data['id'], json_data['id'])
+            'status' : _("Import done - statement id = %s, sheet id = %s") % (json_data['id'], json_data['id'])
         })
             
     except:
@@ -102,18 +126,21 @@ def import_data(request):
         now = datetime.datetime.now()
         objects = []
         new_feature = {}
-        new_feature['table_name'] = settings.TABLE_FAILED_JSON
+        if json_data['input_type'] == 'fauna':
+            new_feature['table_name'] = settings.TABLE_FAILED_JSON_FAUNA
+        else :
+            new_feature['table_name'] = settings.TABLE_FAILED_JSON_MORTALITY
         new_feature['date_import'] = "%d-%d-%d %d:%d:%d" % (now.year, now.month, now.day, now.hour, now.minute, now.second)
         new_feature['json'] = data
         objects.append(new_feature)
-        cursor = sync_db(objects, False)
+        cursor = sync_db(objects)
         id_failed = cursor.fetchone()[0]
 
         # Commit transaction
         commit_transaction();
         
         response_content.append({
-            'status' : "Bad json or data (%d)" % id_failed
+            'status' : _("Bad json or data (%d)") % id_failed
         })
         
     response = HttpResponse()
@@ -169,9 +196,10 @@ def export_data(request, table_name):
     """
     Export table_name data from DataBase to JSON
     """
+    response_content = []
     if not check_token(request):
         response_content.append({
-            'status' : "You're not allowed to retreive information from this webservice"
+            'status' : _("You're not allowed to retreive information from this webservice")
         })
         response = HttpResponse()
         simplejson.dump(response_content, response,
@@ -205,13 +233,11 @@ def check_token(request):
     Check the validity of the token
     """
     
-    # TODO : uncomment this code
-    return True
-    #if request.method == 'POST':
-    #    if request.POST['token']:
-    #        if request.POST['token'] == settings.TOKEN :
-    #            return True
-    #return False
+    if request.method == 'POST':
+        if request.POST['token']:
+            if request.POST['token'] == settings.TOKEN :
+                return True
+    return False
 
         
     
