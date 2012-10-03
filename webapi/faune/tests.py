1be424b861219c63ad39fad40475bec29e481270
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2012-10-03 16:37:52 +0200

    Tests

--- webapi/faune/tests.py
+++ webapi/faune/tests.py
@@ -1,18 +1,24 @@
 
 from django.test import TestCase
 
-import json
-
-
 
 class FauneViewsTestCase(TestCase):
     def test_import(self):
         
         data = open('faune/data.json').read()
-        jsonData = json.dumps(data)
         resp = self.client.post('/import/', {'data': data, 'token': '666'})
 
+        # Check that data has been inserted
+        #select dernier-releve from table_distante;
+        
+        # assertEqual ( data.animal, dernierreleve.animal)
+        
+        # at the end, remove inserted row in database
+        
+        
         self.assertEqual(resp.status_code, 200)
+        
+        
         
     #def test_export_sqlite(self):
     #    resp = self.client.post('/export/sqlite/', {'token': '666'})        
