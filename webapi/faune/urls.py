211aae6c07dc81c5333097cd56db83c30654005f
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2016-07-21 12:04:43 +0200

    Update urls.py

--- webapi/faune/urls.py
+++ webapi/faune/urls.py
@@ -1,17 +1,15 @@
 from django.conf.urls import patterns, include, url
 
-from faune.views import import_data, export_sqlite, export_taxon, export_family, export_unity, export_taxon_unity, export_criterion, export_user, export_classes, export_unity_geojson, export_unity_polygons, check_status, soft_version, soft_download, data_download
+from faune.views import import_data, export_sqlite, export_taxon, export_unity, export_taxon_unity, export_criterion, export_user, export_classes, export_unity_geojson, export_unity_polygons, check_status, soft_version, soft_download, data_download
 
 urlpatterns = patterns('',
     (r'^status/$', check_status),
     (r'^soft_version/$', soft_version),
     (r'^soft_download/(?P<apk_name>[A-Za-z0-9\-\.]+)/$', soft_download),
     (r'^data_download/(?P<mbtiles_name>[A-Za-z0-9\-\.\_]+)/$', data_download),
-    
     (r'^import/$', import_data),
     (r'^export/sqlite/$', export_sqlite),
     (r'^export/taxon/$', export_taxon),
-    (r'^export/family/$', export_family),
     (r'^export/unity/$', export_unity),
     (r'^export/taxon_unity/$', export_taxon_unity),
     (r'^export/criterion/$', export_criterion),
@@ -20,3 +18,4 @@ urlpatterns = patterns('',
     (r'^export/unity_geojson/$', export_unity_geojson),
     (r'^export/unity_polygons/$', export_unity_polygons),
 )
+
