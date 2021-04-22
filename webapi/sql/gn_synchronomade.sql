1f238226a9f80a87692fc993bbcd0e579de5a715
Théo Lechémia <theo.lechemia@ecrins-parcnational.fr>
2021-04-22 16:37:49 +0200

    Faute d'ortographe sinon ça ne marche pas !
    Un taxon déjà vu était tagué "grey", mais il faut que ce soit "gray" pour fonctionner avec l'appli mobile...

--- webapi/sql/gn_synchronomade.sql
+++ webapi/sql/gn_synchronomade.sql
@@ -246,7 +246,7 @@ CREATE VIEW gn_synchronomade.v_color_taxon_area AS
     v_area_taxon.nb_obs,
     v_area_taxon.last_date,
         CASE
-            WHEN (date_part('day'::text, (now() - (v_area_taxon.last_date)::timestamp with time zone)) < (365)::double precision) THEN 'grey'::text
+            WHEN (date_part('day'::text, (now() - (v_area_taxon.last_date)::timestamp with time zone)) < (365)::double precision) THEN 'gray'::text
             ELSE 'red'::text
         END AS color
    FROM (gn_synthese.v_area_taxon v_area_taxon
